26270016398a93897a48cbcee9ee37527c92b177
Valya <V2lentin2005@yandex.ru>
2024-06-22 02:55:12 +0300

    jknk

--- main.cpp
+++ main.cpp
@@ -22,15 +22,15 @@ public:
 
     MATRIX(const std::vector<VECTOR> &data) : data(data), rows_(data.size()), columns_(data[0].size()) {}
 
-    MATRIX(const MATRIX &other) : data(other.data), rows_(other.rows_), columns_(other.columns_) {}
+    MATRIX(const MATRIX &matr) : data(matr.data), rows_(matr.rows_), columns_(matr.columns_) {}
 
     ~MATRIX() {}
 
     // Геттеры, Сеттеры
 
     int getRows() const { return rows_; }
-    int getCols() const { return columns_; }
-    int getDim() const { return getRows() * getCols(); }
+    int getColumns() const { return columns_; }
+    int getDim() const { return getRows() * getColumns(); }
     double getElement(int row, int col) const { return data[row][col]; }
     VECTOR getRow(int row) const { return data[row]; }
     VECTOR getCol(int col) const
@@ -40,7 +40,7 @@ public:
             colMatr[i] = data[i][col];
         return colMatr;
     }
-    std::vector<VECTOR> getMatr() const { return data; }
+    std::vector<VECTOR> getData() const { return data; }
     std::string toString() const
     {
         std::stringstream ss;
@@ -53,29 +53,29 @@ public:
         return ss.str();
     }
 
-    void setElement(int i, int j, double val)
+    void setElement(int row, int col, double val)
     {
-        if (i >= 0 && i < rows_ && j >= 0 && j < columns_)
-            data[i][j] = val;
+        if (row >= 0 && row < rows_ && col >= 0 && col < columns_)
+            data[row][col] = val;
         else
             std::cout << "Invaled row or column index." << std::endl;
     }
-    void setRow(int r, const VECTOR &val)
+    void setRow(int row, const VECTOR &val)
     {
-        if (r >= 0 && r < rows_ && val.size() == columns_)
+        if (row >= 0 && row < rows_ && val.size() == columns_)
         {
             for (int i = 0; i < columns_; ++i)
-                data[r][i] = val[i];
+                data[row][i] = val[i];
         }
         else
             std::cout << "Invalid row index or value size." << std::endl;
     }
-    void setCol(int c, const VECTOR &val)
+    void setCol(int col, const VECTOR &val)
     {
-        if (c >= 0 && c < columns_ && val.size() == rows_)
+        if (col >= 0 && col < columns_ && val.size() == rows_)
         {
             for (int i = 0; i < rows_; ++i)
-                data[i][c] = val[i];
+                data[i][col] = val[i];
         }
         else
             std::cout << "Invalid row index or value size." << std::endl;
@@ -83,96 +83,96 @@ public:
 
     // Перегрузка операторов
 
-    double &operator()(int i, int j) { return data[i][j]; }      // изменение значения эл-та через оператор ()
-    double operator()(int i, int j) const { return data[i][j]; } // получение значения эл-та через оператор ()
-    MATRIX operator+(const MATRIX &other) const
+    double &operator()(int row, int col) { return data[row][col]; }      // изменение значения эл-та через оператор ()
+    double operator()(int row, int col) const { return data[row][col]; } // получение значения эл-та через оператор ()
+    MATRIX operator+(const MATRIX &matr) const
     { // сложение матриц
-        if (rows_ != other.rows_ || columns_ != other.columns_)
+        if (rows_ != matr.rows_ || columns_ != matr.columns_)
             std::cout << "These matrices cannot be folded." << std::endl;
         MATRIX res(rows_, columns_);
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < columns_; ++j)
-                res(i, j) = data[i][j] + other(i, j);
+                res(i, j) = data[i][j] + matr(i, j);
         return res;
     }
-    MATRIX operator-(const MATRIX &other) const
+    MATRIX operator-(const MATRIX &matr) const
     { // вычитание матриц
-        if (rows_ != other.rows_ || columns_ != other.columns_)
+        if (rows_ != matr.rows_ || columns_ != matr.columns_)
             std::cout << "These matrices cannot be subtracted." << std::endl;
         MATRIX res(rows_, columns_);
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < columns_; ++j)
-                res(i, j) = data[i][j] - other(i, j);
+                res(i, j) = data[i][j] - matr(i, j);
         return res;
     }
-    MATRIX operator*(const MATRIX &other) const
+    MATRIX operator*(const MATRIX &matr) const
     { // умножение матриц
-        if (columns_ != other.rows_)
+        if (columns_ != matr.rows_)
             std::cout << "MATRIX dimensions are not compatible for multiplicztion." << std::endl;
-        MATRIX res(rows_, other.columns_);
+        MATRIX res(rows_, matr.columns_);
         for (int i = 0; i < rows_; ++i)
         {
-            for (int j = 0; j < other.columns_; ++j)
+            for (int j = 0; j < matr.columns_; ++j)
             {
                 double sum = 0;
                 for (int k = 0; k < columns_; ++k)
-                    sum += data[i][k] * other(k, j);
+                    sum += data[i][k] * matr(k, j);
                 res(i, j) = sum;
             }
         }
         return res;
     }
-    bool operator==(const MATRIX &other) const
+    bool operator==(const MATRIX &matr) const
     { // равенство матриц
-        if (rows_ != other.rows_ || columns_ != other.columns_)
+        if (rows_ != matr.rows_ || columns_ != matr.columns_)
             return false;
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < columns_; ++j)
-                if (data[i][j] != other(i, j))
+                if (data[i][j] != matr(i, j))
                     return false;
         return true;
     }
-    bool operator!=(const MATRIX &other) const { return !(*this == other); } // неравенство матриц
-    MATRIX operator*(double scalar) const
+    bool operator!=(const MATRIX &matr) const { return !(*this == matr); } // неравенство матриц
+    MATRIX operator*(double digit) const
     { // умножение матрицы на число
         MATRIX res(rows_, columns_);
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < columns_; ++j)
-                res(i, j) = data[i][j] * scalar;
+                res(i, j) = data[i][j] * digit;
         return res;
     }
-    MATRIX operator/(double scalar) const
+    MATRIX operator/(double digit) const
     { // деление матрицы на число
-        if (scalar == 0)
+        if (digit == 0)
             std::cout << "The division cannot be performed." << std::endl;
         MATRIX res(rows_, columns_);
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < columns_; ++j)
-                res(i, j) = data[i][j] / scalar;
+                res(i, j) = data[i][j] / digit;
         return res;
     }
 
     // Работа с Матрицами
 
-    void Resize(int newRows, int newCols, bool preserve = false)
+    void Resize(int newRows, int newColumns, bool preserve = false)
     { // изменение размерности
         if (preserve)
         {
-            if (newRows < rows_ || newCols < columns_)
+            if (newRows < rows_ || newColumns < columns_)
                 std::cout << "Cannot reduce size while preserving data." << std::endl;
-            std::vector<VECTOR> newMatr(newRows, VECTOR(newCols));
+            std::vector<VECTOR> newMatr(newRows, VECTOR(newColumns));
             for (int i = 0; i < rows_; ++i)
                 for (int j = 0; j < columns_; ++j)
                     newMatr[i][j] = data[i][j];
             data = newMatr;
             rows_ = newRows;
-            columns_ = newCols;
+            columns_ = newColumns;
         }
         else
         {
-            data.resize(newRows, VECTOR(newCols));
+            data.resize(newRows, VECTOR(newColumns));
             rows_ = newRows;
-            columns_ = newCols;
+            columns_ = newColumns;
         }
     }
 
@@ -317,21 +317,21 @@ public:
             data[i][0] = vect[i];
         }
     }
-    Vector(const MATRIX &other) : MATRIX(other)
+    Vector(const MATRIX &matr) : MATRIX(matr)
     {
-        if (other.getCols() != 1)
+        if (matr.getColumns() != 1)
             std::cout << "MATRIX must have one column." << std::endl;
     }
     ~Vector() {}
 
     double &operator()(int i) { return data[i][0]; }
     double operator()(int i) const { return data[i][0]; }
-    Vector operator+(const Vector &other) const { return static_cast<Vector>(MATRIX::operator+(other)); }
-    Vector operator-(const Vector &other) const { return static_cast<Vector>(MATRIX::operator-(other)); }
-    Vector operator*(double scalar) const { return static_cast<Vector>(MATRIX::operator*(scalar)); }
-    Vector operator/(double scalar) const { return static_cast<Vector>(MATRIX::operator/(scalar)); }
-    bool operator==(const Vector &other) const { return MATRIX::operator==(other); }
-    bool operator!=(const Vector &other) const { return MATRIX::operator!=(other); }
+    Vector operator+(const Vector &matr) const { return static_cast<Vector>(MATRIX::operator+(matr)); }
+    Vector operator-(const Vector &matr) const { return static_cast<Vector>(MATRIX::operator-(matr)); }
+    Vector operator*(double digit) const { return static_cast<Vector>(MATRIX::operator*(digit)); }
+    Vector operator/(double digit) const { return static_cast<Vector>(MATRIX::operator/(digit)); }
+    bool operator==(const Vector &matr) const { return MATRIX::operator==(matr); }
+    bool operator!=(const Vector &matr) const { return MATRIX::operator!=(matr); }
 };
 
 int main()
